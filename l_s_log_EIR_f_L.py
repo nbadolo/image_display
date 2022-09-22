5bc7e0f2fc1b96f8bc3383384cca1ca386bbcbca
BADOLO Aymard <baymardn@gmail.com>
2022-09-22 11:11:26 +0200

    add fits code

--- l_s_log_EIR_f_L.py
+++ l_s_log_EIR_f_L.py
@@ -24,13 +24,16 @@ import numpy as np
 # Definitions bloc:
     file_path_l: chemin gros tableau
     file_path_s: chemin du petit tableau
+    file_path_r: chein des étoiles resolues
 """
 
 file_path_l = '/home/nbadolo/Bureau/Aymard/These/for_papers/large_log_par.ods'
 file_path_s = '/home/nbadolo/Bureau/Aymard/These/for_papers/short_log_par.ods'
+file_path_r = '/home/nbadolo/Bureau/Aymard/These/for_papers/resol_log_par.ods'
 
 df_l = pd.read_excel(file_path_l)
 df_s = pd.read_excel(file_path_s)
+df_r = pd.read_excel(file_path_r)
 # xmin = np.min(df["J-Ks"]) 
 # xmax = np.max(df["J-Ks"])
 # ymin = np.min(df["E_IR"])
@@ -84,14 +87,16 @@ fig.set_size_inches(18.5, 10, forward = True)
 plt.subplot(2,2,1)
 plt.plot(df_l["L"], df_l["E_IR"], 'b+')
 plt.plot(df_s["L"], df_s["E_IR"], 'r+')
-plt.legend(["large table", "the sample"])
+plt.plot(df_r["L"], df_r["E_IR"], 'k+')
+plt.legend(["large table", "the sample", "resolved"])
 plt.xlabel('L', size=10)
 plt.ylabel('E_IR', size=10)
 
 plt.subplot(2,2,2)
 plt.plot(df_l["J-K"], df_l["E_IR"], 'b+')
 plt.plot(df_s["J-K"], df_s["E_IR"], 'r+')
-plt.legend(["large table", "the sample"])
+plt.plot(df_r["J-K"], df_r["E_IR"], 'k+')
+plt.legend(["large table", "the sample", "resolved"])
 plt.xlabel('J-K', size=10)
 plt.ylabel('E_IR', size=10)
 
@@ -99,24 +104,26 @@ plt.ylabel('E_IR', size=10)
 plt.subplot(2,2,3)
 plt.plot(df_l["Teff"], df_l["E_IR"], 'b+')
 plt.plot(df_s["Teff"], df_s["E_IR"], 'r+')
-plt.legend(["large table", "the sample"])
+plt.plot(df_r["Teff"], df_r["E_IR"], 'k+')
+plt.legend(["large table", "the sample", "resolved"])
 plt.xlabel('Teff', size=10)
 plt.ylabel('E_IR', size=10)
 
 plt.subplot(2,2,4)
 plt.plot(df_l["P"], df_l["E_IR"], 'b+')
 plt.plot(df_s["P"], df_s["E_IR"], 'r+')
-plt.legend(["large table", "the sample"])
+plt.plot(df_r["P"], df_r["E_IR"], 'k+')
+plt.legend(["large table", "the sample", "resolved"])
 plt.xlabel('P', size=10)
 plt.ylabel('E_IR', size=10)
 
 #plt. title( label = "Excess infrared in relation to key parameters" ,fontsize = 12 ,color = "k" )
 
-plt.savefig('/home/nbadolo/Bureau/Aymard/These/for_papers/plots/log_paper_plots/' + 'large_log_E_IR.pdf', 
+plt.savefig('/home/nbadolo/Bureau/Aymard/These/for_papers/plots/log_paper_plots/' + 'l_s_log_E_IR.pdf', 
                 dpi=100, bbox_inches ='tight')
 
 
-plt.savefig('/home/nbadolo/Bureau/Aymard/These/for_papers/plots/log_paper_plots/' + 'large_log_E_IR.png', 
+plt.savefig('/home/nbadolo/Bureau/Aymard/These/for_papers/plots/log_paper_plots/' + 'l_s_log_E_IR.png', 
                 dpi=100, bbox_inches ='tight')
 plt.tight_layout()
 plt.show()
--- retuct_all_stars.py
+++ retuct_all_stars.py
@@ -14,22 +14,22 @@ import deconv
 lst_star = os.listdir('/home/nbadolo/Bureau/Aymard/Donnees_sph/log/')
 lst_star.sort()
 lst_len = len(lst_star)
-print(lst_star)
+#print(lst_star)
 
 for i in range(lst_len): # affiche uniquement irc_10420
    if i == 5 :    
        log_irc_images.log_image(lst_star[i], 'alone')
        log_irc_images.log_image(lst_star[i], 'both')   
    
-   else :  # Affiche toutes les étoiles qui n'ont pas de psf
-       if i == 0 or i == 3 or i == 14 or i == 19 or i == 21 or i == 22:
-           log_agb_images_wp.log_image(lst_star[i], 'alone')
-           log_agb_images_wp.log_image(lst_star[i], 'both')
+   # else :  # Affiche toutes les étoiles qui n'ont pas de psf
+   #     if i == 0 or i == 3 or i == 14 or i == 19 or i == 21 or i == 22:
+   #         log_agb_images_wp.log_image(lst_star[i], 'alone')
+   #         log_agb_images_wp.log_image(lst_star[i], 'both')
        
-       else: # Affiche toutes les étoiles normales
-           log_agb_images.log_image(lst_star[i], 'alone')
-           log_agb_images.log_image(lst_star[i], 'both')
+   #     else: # Affiche toutes les étoiles normales
+   #         log_agb_images.log_image(lst_star[i], 'alone')
+   #         log_agb_images.log_image(lst_star[i], 'both')
            
-   if i == 9 or i == 10 or i == 13 or i == 15 or i == 24 :
-       deconv.log_image(lst_star[i], 'alone')
-       deconv.log_image(lst_star[i], 'both')
+   # if i == 9 or i == 10 or i == 13 or i == 15 or i == 24 :
+   #     deconv.log_image(lst_star[i], 'alone')
+   #     deconv.log_image(lst_star[i], 'both')
